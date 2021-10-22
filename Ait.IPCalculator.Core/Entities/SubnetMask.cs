1be03ca0544b331e2696f0339ea25dba23a26bbf
Nataliya <nataliya.danyoukova@student.howest.be>
2021-10-22 15:00:52 +0200

    Add SubnetMask class

---
+++ Ait.IPCalculator.Core/Entities/SubnetMask.cs
@@ -0,0 +1,20 @@
+﻿namespace Ait.IPCalculator.Core.Entities
+{
+    public class SubnetMask : IP
+    {
+
+        public int prefix { get; set; } = 0;
+
+        public SubnetMask()
+        {
+            prefix++;
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"{ part1}.{part2}.{part3}.{part4}/({prefix})";
+        }
+    }
+}
