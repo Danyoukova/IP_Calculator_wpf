836dad1cc2b7cd4b24768e37d8853a5f2a2a6dad
Nataliya <nataliya.danyoukova@student.howest.be>
2021-10-22 14:59:06 +0200

    Add IP class

---
+++ Ait.IPCalculator.Core/Entities/IP.cs
@@ -0,0 +1,10 @@
+﻿namespace Ait.IPCalculator.Core.Entities
+{
+    public class IP
+    {
+        public int part1 { get; set; }
+        public int part2 { get; set; }
+        public int part3 { get; set; }
+        public int part4 { get; set; }
+    }
+}
